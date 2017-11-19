aedfe17ca49642aba36488b68902f14bf95f4c4b
mohamed179 <mohamed77zaki@gmail.com>
2017-11-19 15:44:05 +0200

    Separate header and footer from index.php and adding html base code for signup.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta name="charset" content="UTF-8"/>
+	<meta name="viewport" content="width=device-width, inital-scale=1.0"/>
+	<meta name="description" content="A good chat system"/>
+	<base target="_blank"/>
+	<title>Chat System</title>
+	<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+	<header>
+	</header>
--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-	<meta name="charset" content="UTF-8"/>
-	<meta name="viewport" content="width=device-width, inital-scale=1.0"/>
-	<meta name="description" content="A good chat system"/>
-	<base target="_blank"/>
-	<title>Chat System</title>
-	<link rel="stylesheet" href="styles/style.css">
-</head>
-<body>
-	<header>
-	</header>
+<?php include 'header.php'; ?>
 	<section>
 	</section>
-	<footer>
-	</footer>
-</body>
-</html>
+<?php include 'footer.php'; ?>
