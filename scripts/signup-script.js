7e5c9b8bf422ef351a4b59e0327b68bbe15bb553
mohamed179 <mohamed77zaki@gmail.com>
2017-11-22 00:02:04 +0200

    Adding html, css and javascript code for sign up page

---
+++ scripts/signup-script.js
@@ -0,0 +1,64 @@
+var currentTab = 0;
+showTab(currentTab);
+nxtPrev(0);
+
+function showTab () {
+	var tabs = document.getElementsByClassName("tab");
+	for (i = 0; i < tabs.length; i++) {
+		if (i == currentTab)
+			tabs[i].style.display = "block";
+		else
+			tabs[i].style.display = "none";
+	}
+}
+
+function checkInputs () {
+	var tabs = document.getElementsByClassName("tab");
+	var inputs = tabs[currentTab].getElementsByTagName("input");
+	var check = true;
+	for (i = 0; i < inputs.length; i++) {
+		if (inputs[i].value == "" && inputs[i].name != "phone") {
+			inputs[i].className = "invalid"
+			check = false;
+		}
+	}
+	return check;
+}
+
+function validatePass () {
+	var tabs = document.getElementsByClassName("tab");
+	var inputs = tabs[currentTab].getElementsByTagName("input");
+	var pass = inputs[1].value;
+	var repass = inputs[2].value;
+	if (pass != repass) {
+		inputs[2].className = "invalid";
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function validatefrom () {
+	return (checkInputs() && validatePass());
+}
+
+function nxtPrev (x) {
+	if (x <= 0 || checkInputs()) {
+		currentTab += x;
+		showTab(currentTab);
+		if (currentTab == 0) {
+			document.getElementById("nxtBtn").style.display = "block";
+			document.getElementById("prevBtn").style.display = "none";
+			document.getElementById("submitBtn").style.display = "none";
+		}
+		else if (currentTab == 1) {
+			document.getElementById("nxtBtn").style.display = "block";
+			document.getElementById("prevBtn").style.display = "block";
+			document.getElementById("submitBtn").style.display = "none";
+		} else {
+			document.getElementById("nxtBtn").style.display = "none";
+			document.getElementById("prevBtn").style.display = "block";
+			document.getElementById("submitBtn").style.display = "block";
+		}
+	}
+}
--- signup.php
+++ signup.php
@@ -1,4 +1,45 @@
 <?php include 'header.php'; ?>
+<link rel="stylesheet" href="styles/signup-style.css">
 	<section>
+		<h1>Sign up</h1>
+		<div id="signup">
+			<form action="includes/signup.inc.php" method="POST" onsubmit="return validatefrom()">
+				<div class="tab">
+					<h2>Personal</h2>
+					<label for="fname"><b>Firstname :</b></label>
+					<input type="text" name="fname" placeholder="Enter firstname.." oninput="this.className = ''" />
+					<label for="lname"><b>Lastname :</b></label>
+					<input type="text" name="lname" placeholder="Enter lastname.." oninput="this.className = ''" />
+					<label for="dob"><b>Date of birth :</b></label>
+					<input type="date" name="dob" />
+					<label for="gdr"><b>Gender :</b></label>
+					<select name="gdr">
+						<option>Male</option>
+						<option>Female</option>
+					</select>
+				</div>
+				<div class="tab">
+					<h2>Contact</h2>
+					<label for="email"><b>E-mail :</b></label>
+					<input type="email" name="email" placeholder="Enter e-mail.." oninput="this.className = ''" />
+					<label for="phone"><b>Phone <span>(not required)</span> :</b></label>
+					<input type="tel" name="phone" placeholder="Enter phone.." />
+				</div>
+				<div class="tab">
+					<h2>Login info</h2>
+					<label for="uname"><b>Username :</b></label>
+					<input type="text" name="uname" placeholder="Enter username.." oninput="this.className = ''" />
+					<label for="pswd"><b>Password :</b></label>
+					<input type="password" name="pswd" placeholder="Enter password.." oninput="this.className = ''" />
+					<input type="password" name="repswd" placeholder="Re-enter password.." oninput="this.className = ''" />
+				</div>
+				<div id="signup-buttons">
+					<button id="submitBtn" type="submit" name="submit">Sign up</button>
+					<button id="nxtBtn" type="button" onclick="nxtPrev(1)">Next</button>
+					<button id="prevBtn" type="button" onclick="nxtPrev(-1)" style="background: #bbbbbb;">Previous</button>
+				</div>
+			</form>
+		</div>
 	</section>
+	<script src="scripts/signup-script.js"></script>
 <?php include 'footer.php'; ?>
